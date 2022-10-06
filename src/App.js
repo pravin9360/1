1386bcf012867916f98dd3b8ab3abb22ff9959c9
pravin9360 <pravinloganathan123@gmail.com>
2022-10-06 14:49:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import './Style.css';
+import FunctionalCount from './button.js'
+import images from './Images/images.jfif'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={images} alt="Img"/>
+        <FunctionalCount />
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; 
